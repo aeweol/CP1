ec56ea66051c3fa6c4d619b01fbcfca27560891f
aeweol <hongik1228@naver.com>
2022-11-12 18:46:41 +0900

    용량문제로 재업로드

--- Load_Data.py
+++ Load_Data.py
@@ -20,7 +20,7 @@ def load_image_dataset_from_directory(main_dir, train_dir, valid_dir, test_dir):
         batch_size=32,
         seed=42,    # validation_split을 위한 seed 고정
         validation_split = 0.2,
-        subset="training",
+        subset="validation",
         )
     test_ds = keras.utils.image_dataset_from_directory(
         directory=base_dir+main_dir+test_dir,
