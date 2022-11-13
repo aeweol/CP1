ef9b67c0edaba4eea8504fb1a81b5c19528f338f
aeweol <hongik1228@naver.com>
2022-11-13 10:03:33 +0900

    다양한 방법의 이미지로드 시도 중

--- Load_Data.py
+++ Load_Data.py
@@ -1,5 +1,7 @@
 import os
 from tensorflow import keras
+import numpy as np
+from PIL import Image
 
 base_dir = os.getcwd()
 
@@ -14,7 +16,7 @@ def load_image_dataset_from_directory(main_dir, train_dir, valid_dir, test_dir):
         subset="training",
         )
     valid_ds = keras.utils.image_dataset_from_directory(
-        directory=base_dir+main_dir+valid_dir,
+        directory=base_dir+main_dir+train_dir,
         labels='inferred',
         label_mode='categorical',
         batch_size=32,
@@ -29,3 +31,27 @@ def load_image_dataset_from_directory(main_dir, train_dir, valid_dir, test_dir):
         batch_size=32,
         )
     return train_ds, valid_ds, test_ds
+
+
+
+def load_image_batch_size_one(
+    main_dir, train_dir, test_dir,
+    label_names,
+    target_size=None,
+    interpolation='nearest',
+    keep_aspect_ratio=False
+    ):
+    num_of_labels = len(label_names)
+    label_list = np.zeros(num_of_labels)
+    for i in range(num_of_labels):
+        label = np.zeros(num_of_labels)
+        label[i] = 1
+        label_list.append(label)
+    
+    testing_image = keras.utils.image.load_img(
+        path=base_dir+main_dir+train_dir,
+        target_size=target_size,
+        interpolation=interpolation,
+        keep_aspect_ratio=keep_aspect_ratio)
+
+    
