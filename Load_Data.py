99eea3493532c0aa80c812aeebb88584da5a64bd
aeweol <hongik1228@naver.com>
2022-11-15 11:53:45 +0900

    이미지 파일명 통일 및 이미지 로드 옵션(배치 당 1개의 자유크기 이미지) 추가코드 작성 중

--- Load_Data.py
+++ Load_Data.py
@@ -1,6 +1,7 @@
 import os
 from tensorflow import keras
 import numpy as np
+import random
 from PIL import Image
 
 base_dir = os.getcwd()
@@ -35,23 +36,35 @@ def load_image_dataset_from_directory(main_dir, train_dir, valid_dir, test_dir):
 
 
 def load_image_batch_size_one(
-    main_dir, train_dir, test_dir,
+    main_dir, train_dir,
     label_names,
     target_size=None,
     interpolation='nearest',
     keep_aspect_ratio=False
     ):
     num_of_labels = len(label_names)
-    label_list = np.zeros(num_of_labels)
-    for i in range(num_of_labels):
+    label_list = {}
+    for i, label_name in enumerate(label_names):
         label = np.zeros(num_of_labels)
         label[i] = 1
-        label_list.append(label)
-    
-    testing_image = keras.utils.image.load_img(
-        path=base_dir+main_dir+train_dir,
-        target_size=target_size,
-        interpolation=interpolation,
-        keep_aspect_ratio=keep_aspect_ratio)
+        #label_list[label_name] = label
+        label_list[label_name] = i
 
+    batch_set = []
+    for label_name in label_names:
+        label_dir = f'/{label_name}'
+        for i in range(1,51):
+            if i < 10:
+                file_num = '0'+str(i)
+            else:
+                file_num = str(i)
+            file_name = label_name+file_num
+            this_image = keras.preprocessing.image.load_img(
+                path=base_dir+main_dir+train_dir+label_dir+'/'+file_name+'.jpg',
+                target_size=target_size,
+                interpolation=interpolation,
+                keep_aspect_ratio=keep_aspect_ratio)
+            image_arr = keras.preprocessing.image.img_to_array(this_image)
+            batch_set.append([image_arr, label_list[label_name]])
     
+    return batch_set
